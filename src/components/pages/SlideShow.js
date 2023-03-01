d1fdd9f9a10f26f770360ce051f154572122639d
Bruno Bronzeri <brunobronzeri@icloud.com>
2023-03-01 15:01:33 -0300

    carouseltimer

--- src/components/pages/SlideShow.js
+++ src/components/pages/SlideShow.js
@@ -1,58 +1,54 @@
-import { useRef, useState } from 'react';
-import styles from './SlideShow.module.css'
+import { useCallback, useEffect, useRef, useState } from 'react';
+import styles from './SlideShow.module.css';
 
 function SlideShow({ images }) {
-
     const carousel = useRef();
     const [count, setCount] = useState(0);
-
-    // timer section
+    const [timer, setTimer] = useState(0);
     
+    const Incremet = useCallback(
+        (delta) => {
+            clearTimeout(timer);
+            if (!carousel.current) {
+                return;
+            }
 
-    //
+            const width = carousel.current.offsetWidth;
 
-    function Incremet(delta) {
-        if (!carousel.current) {return};
+            if (count + delta > images.length - 1) {
+                setCount(0);
+                carousel.current.scrollTo(0, 0); //scroll all way to zero
+                return;
+            } else if (count + delta < 0) {
+                setCount(images.length - 1);
+                carousel.current.scrollTo(
+                    carousel.current.scrollLeft + width * images.length - 1,
+                    0
+                );
+                return;
+            }
 
-        const width = carousel.current.offsetWidth;        
-
-        if (count + delta > images.length -1) {
-            setCount(0);
-            carousel.current.scrollTo(0, 0); //scroll all way to zero
-            return;
-        }
-        
-        else if (count + delta < 0) {
-            setCount(images.length - 1);
             carousel.current.scrollTo(
-                carousel.current.scrollLeft + width * images.length - 1,
+                carousel.current.scrollLeft + width * delta,
                 0
             );
-            return;    
-        }
+            setCount((c) => c + delta);
+        },
+        [count, images.length,timer]
+    );
 
-        carousel.current.scrollTo(
-            carousel.current.scrollLeft + width * delta, 0
-        );
-        setCount(c => c + delta);
-    }
+    useEffect(() => {
+        setTimer(setTimeout(() => Incremet(1), 5000));
+    }, [count]);
 
-    return(
+    return (
         <div className={styles.container}>
+            <div className={styles.btn_l} onClick={() => Incremet(-1)} />
+            <div className={styles.btn_r} onClick={() => Incremet(1)} />
 
-            <div className={styles.btn_l}
-                onClick={() => Incremet(-1)}
-            />
-            <div className={styles.btn_r}
-                onClick={() => Incremet(1)}
-            />
-                
             <div className={styles.carousel} ref={carousel}>
                 {images.map((img, idx) => (
-                    <div
-                        className={styles.item}
-                        key={`${idx}-${img.title}`}
-                    >
+                    <div className={styles.item} key={`${idx}-${img.title}`}>
                         <img src={img.src} alt={img.title} />
                         <p>{img.title}</p>
                     </div>
@@ -62,4 +58,4 @@ function SlideShow({ images }) {
     );
 }
 
-export default SlideShow
+export default SlideShow;
