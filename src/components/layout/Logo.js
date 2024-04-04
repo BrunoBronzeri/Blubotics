4e919df909178cd9e0da4efb69141185049d67f3
Bruno Bueno Bronzeri <brunobronzeri@icloud.com>
2024-04-04 15:16:50 -0300

    fix path name to home

--- src/components/layout/Logo.js
+++ src/components/layout/Logo.js
@@ -7,7 +7,7 @@ import { FaFacebook, FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa'
 function Logo() {
     return (
         <div className={styles.logo}>
-            <Link to='/Blubotics'>
+            <Link to=''>
                 <img src={logo} alt='Blubotics' />
             </Link>
             <div>
