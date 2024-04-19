d0ed679b8ce0b7735c16f2c488032ecad9c45be1
Bruno Bueno Bronzeri <brunobronzeri@icloud.com>
2024-04-19 14:58:40 -0300

    projects/Robo

--- src/components/pages/Robot.js
+++ src/components/pages/Robot.js
@@ -8,12 +8,16 @@ function Robot() {
             <div className={styles.card}>
                 <img src={LF} alt='pic'/>
                 <div>
-                    <h2>Nome: Frank</h2>
+                    <h2>Frank</h2>
                     <h3>Categoria: Seguidor de Linha</h3>
                 </div>
             </div>
             <div>
-                <h1>Definir layout</h1>
+                <h1>Robô Seguidor de Linha</h1>
+                <p>Primeiro desenvolvimento da equipe. O seguidor de linha conta
+                    com um Arduino Mega e um Shield de circuito impresso desenvolvido
+                    pela própria equipe.
+                </p>
             </div>
         </div>
     )
