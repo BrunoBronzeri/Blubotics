44160793fa984d0f6a5b7af40c2afab7d8b7216a
Bruno Bueno Bronzeri <brunobronzeri@icloud.com>
2024-10-02 10:58:09 -0300

    add new prof pic and map update

--- src/components/layout/FooterPc.js
+++ src/components/layout/FooterPc.js
@@ -55,14 +55,14 @@ function FooterPc() {
                     <li>
                         <p>
                             <FaMapMarked />
-                            <a href='https://goo.gl/maps/65iC7vnZtxhU6Zpk7'>
-                                Rua João Pessoa, 2514
+                            <a href='https://maps.app.goo.gl/RinAGu8mt26UWpKA6'>
+                                Rua Eng. Udo Deeke, 485 - 
                                 Blumenau-SC
                             </a>
                         </p>
                     </li>
                     <li>
-                        <iframe title='mapa' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3557.4507044761813!2d-49.10106838920194!3d-26.92092269344905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94df1936e20a59d9%3A0x75a7afa5aef7bb6!2sUFSC%20-%20Universidade%20Federal%20de%20Santa%20Catarina%20-%20Campus%20Blumenau!5e0!3m2!1spt-BR!2sbr!4v1713450081374!5m2!1spt-BR!2sbr"
+                        <iframe title='mapa' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3558.9989932214776!2d-49.09764898933214!3d-26.87177307657423!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94df1e58ef95d901%3A0x901dc80709c3d468!2sR.%20Eng.%20Udo%20Deeke%2C%20485%20-%20Salto%20Norte%2C%20Blumenau%20-%20SC%2C%2089065-100!5e0!3m2!1spt-BR!2sbr!4v1727877034869!5m2!1spt-BR!2sbr"
                             width="200" height="100"  allowfullscreen=""
                             loading="lazy" referrerpolicy="no-referrer-when-downgrade">
                         </iframe>
--- src/components/pages/Members.js
+++ src/components/pages/Members.js
@@ -1,21 +1,25 @@
 import styles from './Members.module.css'
 import { FaInstagram, FaLinkedin } from 'react-icons/fa'
 
-import profile from '../../images/profilepic.png'
 import bruno from '../../images/profile/BrunoB.jpg'
+import matsuo from '../../images/profile/MarcosM.png'
 
 function Mates({ members }) {
     return (
         <div className={styles.container}>
             <div className={styles.profile}>
                 <div className={styles.card}>
-                    <img src={profile} alt='pic'/>
+                    <img src={matsuo} alt='pic'/>
                     <div>
                         <h2>Marcos Matsuo</h2>
                         <h3>Prof. Oreintador</h3>
                         <ul>
                             <li><FaLinkedin /></li>
-                            <li><FaInstagram /></li>
+                            <li>
+                                <a href='https://www.instagram.com/matsuo.marcos/'>
+                                    <FaInstagram />
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </div>
